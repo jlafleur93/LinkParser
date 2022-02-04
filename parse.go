537ab893e3500a5b8cd8e19c7d20ab9e5c37862f
jlafleur93 <josephlafleur@hotmail.com>
2022-02-04 05:37:34 -0700

    added parsing

--- parse.go
+++ parse.go
@@ -1,11 +1,27 @@
 package link
 
 import "io"
+import (
+	"golang.org/x/net/html"
+	"fmt"
+)
 type Link struct {
 	Href string
 	Text string
 }
 
 func Parse(r io.Reader) ([]Link, error){
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string){
+	fmt.Println(padding, n.Data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding + " ")
+	}
 }
